0950e160f49deab48613ca71ecdfc01512913f01
Francesco Iorio <ioriofre_99@hotmail.it>
2024-04-05 18:27:57 +0200

    Updating

---
+++ src/main/java/com/freiorio/exercises/collections/CollatzSequence.java
@@ -0,0 +1,10 @@
+package com.freiorio.exercises.collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CollatzSequence {
+    public static List<Long> collatzSequence(long n) {
+        return new ArrayList<>();
+    }
+}
